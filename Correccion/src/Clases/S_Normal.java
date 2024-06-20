7f26d11f4efb6bdd2da4cd734a55103e3a62e22c
StalinMuela <stalinsangucho87@gmail.com>
2024-06-20 08:10:52 -0500

    Cambios

--- Correccion/src/Clases/S_Normal.java
+++ Correccion/src/Clases/S_Normal.java
@@ -2,9 +2,18 @@ package Clases;
 
 public class S_Normal extends Servicios{
     String tipoasiento;
+    String lugarpasillo;
     Boolean espacioMaleta;
     Boolean maletaAdicional;
 
+    public S_Normal() {
+    }
 
-
+    public S_Normal(String ruta, String servicio, double pasajetotal, String nombre, int numasientos, String tiposervicio, String tipoasiento, String lugarpasillo, Boolean espacioMaleta, Boolean maletaAdicional) {
+        super(ruta, servicio, pasajetotal, nombre, numasientos, tiposervicio);
+        this.tipoasiento = tipoasiento;
+        this.lugarpasillo = lugarpasillo;
+        this.espacioMaleta = espacioMaleta;
+        this.maletaAdicional = maletaAdicional;
+    }
 }
--- Correccion/src/Clases/S_Vip.java
+++ Correccion/src/Clases/S_Vip.java
@@ -6,4 +6,13 @@ public class S_Vip extends Servicios{
     int maletas;
     Boolean maletaadicional;
 
+    public S_Vip() {
+    }
+
+    public S_Vip(String ruta, String servicio, double pasajetotal, String nombre, int numasientos, String tiposervicio, String servicioAdicionales, int maletas, Boolean maletaadicional) {
+        super(ruta, servicio, pasajetotal, nombre, numasientos, tiposervicio);
+        this.servicioAdicionales = servicioAdicionales;
+        this.maletas = maletas;
+        this.maletaadicional = maletaadicional;
+    }
 }
