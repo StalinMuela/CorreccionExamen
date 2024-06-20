ec835cb96bce386b6ced7f99c2cba566dc941e74
Mateo Moran <mateoextra93@gmail.com>
2024-06-20 08:08:35 -0500

    1

--- Correccion/src/Clases/Servicios.java
+++ Correccion/src/Clases/Servicios.java
@@ -1,5 +1,24 @@
 package Clases;
 
 public class Servicios extends Rutas{
+    String tiposervicio;
+
+    public Servicios() {
+    }
+    public Servicios(String ruta, String servicio, double pasajetotal, String nombre, int numasientos, String tiposervicio) {
+        this.tiposervicio = tiposervicio;
+    }
+
+    public String getTiposervicio() {
+        return tiposervicio;
+    }
+
+    public void setTiposervicio(String tiposervicio) {
+        this.tiposervicio = tiposervicio;
+    }
+
+    public void mostrarServicio() {
+        System.out.println("Tipo de Servicio: " + tiposervicio);
+    }
     
 }
